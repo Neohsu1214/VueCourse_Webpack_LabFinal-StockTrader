faef43e16a387965337ae0ae23750f59593cdde0
Neo Hsu <neohsu@Neode-Mac-mini.local>
2020-04-15 23:48:49 +0800

    Stock雛型完成，並套用到 Stocks以 v-for 顯示出來

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,12 +1,26 @@
 <template>
     <div>
-        <h1>The Stocks Component</h1>
+        <app-stock v-for="(stock, idx) in stocks" :key="idx"></app-stock>
     </div>
 </template>
 
 <script>
+import Stock from './Stock.vue';
+
 export default {
-    
+    data: function() { // ES6 下可以簡寫成 data() {}
+        return {
+            stocks: [
+                {id: 1, name: 'BMW', price: 110},
+                {id: 2, name: 'Google', price: 200},
+                {id: 3, name: 'Apple', price: 250},
+                {id: 4, name: 'Twitter', price: 8}
+            ]
+        }
+    }, 
+    components: {
+        appStock: Stock
+    }
 }
 </script>
 
