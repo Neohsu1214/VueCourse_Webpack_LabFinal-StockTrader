0dedba83384bb82e65d0011c589b2539d6a8bf60
Neo Hsu <neohsu@Neode-Mac-mini.local>
2020-04-15 22:29:48 +0800

    建立基本的 routes 規則

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+// 加入 vue-route 的設定
+import { routes } from './routes.js'
+
+Vue.use(VueRouter);
+
+// 設定 VueRouter 物件 
+const router = new VueRouter({
+  mode: 'history',
+  routes: routes // ES6支援同名簡寫，此行可以減寫成 routes
+});
 
 new Vue({
   el: '#app',
+  router: router, // 將 VueRouter物件加到 Vue instance 中。 ES6支援同名簡寫，此行可以減寫成 router
   render: h => h(App)
 })
