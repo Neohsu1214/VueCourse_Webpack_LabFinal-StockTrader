40ead12657cccfa178363c8ce9d6c075cfd4ef80
Neo Hsu <neohsu@Neode-Mac-mini.local>
2020-04-19 08:59:48 +0800

    透過 transition 幫頁面切換加上動畫

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
         <app-header></app-header>
         <div class="row">
             <div class="col-xs-12">
-                <router-view></router-view>
+                <transition name="slide" mode="out-in">
+                    <router-view></router-view>
+                </transition>
             </div>
         </div>
     </div>
@@ -26,4 +28,40 @@
     body  {
         padding: 30px;
     }
+
+
+    /* 建立一個 transition 名為 slide 的classes */
+    .slide-enter {
+        /* transform: translateY(20px); 因為與slide-in 起始位置相同，可不用寫 */
+    }
+    .slide-enter-active {
+        animation: slide-in 200ms ease-out forwards;
+    }
+    .slide-leave {
+        
+    }
+    .slide-leave-active {
+        animation: slide-out 200ms ease-out forwards;
+    }
+
+    @keyframes slide-in {
+        from {
+            transform: translateY(-30px);
+            opacity: 0;
+        }
+        to {
+            transform: translateY(0);
+            opacity: 1;
+        }
+    }
+    @keyframes slide-out {
+        from {
+            transform: translateY(0);
+            opacity: 1;
+        }
+        to {
+            transform: translateY(-30px);
+            opacity: 0;
+        }
+    }    
 </style>
