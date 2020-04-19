c25d939f747a1c1c7d6677f2e7cc7901ba7f5884
Neo Hsu <neohsu@Neode-Mac-mini.local>
2020-04-19 23:52:53 +0800

    加註箭頭函式使用說明

--- src/store/firebase_actions.js
+++ src/store/firebase_actions.js
@@ -4,6 +4,11 @@
 import Vue from 'vue';
 
 export const loadData = ({ commit }) => {
+    /**
+     * 因為只有一個context在進行，而且我們只需要用到 context.commit 這個 function
+     * ES6支援將 (context) => { context.commit('mutations函式名稱'); } 簡化語法為 ({commit}) => {commit('mutations函式名稱')} 
+     * 箭頭函式（）=>的用法與簡化規則詳見：https://developer.mozilla.org/zh-TW/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+     */
     Vue.http.get('data.json')
         .then(response => response.json()) // 將 response.json() 的結果回傳
         .then(data => { // 此時 data 即為 response.json() 的結果，已經是 json 格式的內容，可以進行處理
